9095ed9de8f23137c03ea3a0484999abcdee96e2
kikeriveros <k1k3r1v3r052029@gmail.com>
2023-11-16 19:47:29 -0500

    realizando pruebas

--- api.js
+++ api.js
@@ -1,5 +1,16 @@
+/*
 const path = require("path")
 const filename = 'file1.txt';
 const fullpath = path.join(___dirname, filename);
 console.log ('LA NUEVA RUTA ES:', fullpath)
 console.log (path.dirname(fullpath))
+*/
+/*
+const fs = require('fs')
+fs.writeFileSync('texto1.txt', 'archivo creado desde NODE')
+*/
+const fs = require('fs')
+fs.writeFile('texto1.txt', 'realizando pruebas en NODE', function (){
+    console.log("Archivo creado")
+})
+console.log("Ultima linea")
