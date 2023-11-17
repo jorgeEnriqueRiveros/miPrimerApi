2d804841ac5748413956ec8ef5ae097e162f418c
kikeriveros <k1k3r1v3r052029@gmail.com>
2023-11-16 19:16:02 -0500

    realizando codigo

--- api.js
+++ api.js
@@ -1,2 +1,5 @@
-const os = require("os")
-console.log(os.machine())
+const path = require("path")
+const filename = 'file1.txt';
+const fullpath = path.join(___dirname, filename);
+console.log ('LA NUEVA RUTA ES:', fullpath)
+console.log (path.dirname(fullpath))
